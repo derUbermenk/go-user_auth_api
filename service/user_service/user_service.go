438f371be3e16917106307ccad29311ff6ba7402
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-05 02:09:03 +0800

    add NewUserRequestAttribute definitions

--- service/user_service/user_service.go
+++ service/user_service/user_service.go
@@ -1,6 +1,10 @@
 package user_service
 
-type NewUserRequest struct{}
+type NewUserRequest struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
 
 type UserService interface {
 	CreateUser(newUserRequest NewUserRequest) (user interface{}, status bool, err error)
