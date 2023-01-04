49ba183cb2a7e7ae2670d36bddbcd87eb5fd2997
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-04 23:37:32 +0800

    write create function for user_service

--- handler/users_handler/users_handler.go
+++ handler/users_handler/users_handler.go
@@ -1,1 +1,57 @@
 package users_handler
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Create(user_service api.UserService) func(c *gin.Context) {
+	type CreateResponse struct {
+		User_ID int `json:user_id`
+	}
+
+	return func(c *gin.Context) {
+		var new_user_request api.NewUserRequest
+
+		_ = c.ShouldBindJSON(&new_user_request)
+
+		/*
+			if err != nil {
+				log.Printf("Handler Error: %v", err)
+				c.JSON(
+					http.StatusBadRequest,
+					nil,
+				)
+			}
+		*/
+
+		user, status, err := user_service.CreateUser(new_user_request)
+
+		if err != nil {
+			log.Printf("Service Error: %v", err)
+			c.JSON(
+				http.StatusInternalServerError,
+				nil,
+			)
+
+			return
+		}
+
+		if status == false {
+			c.JSON(
+				http.StatusBadRequest,
+				nil,
+			)
+
+			return
+		}
+
+		c.JSON(
+			http.StatusOK,
+			user,
+		)
+	}
+
+}
