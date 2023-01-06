09b285437783ec047a54f0e066ea9faa300cdd3d
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-06 17:20:06 +0800

    fix imports for session service

--- handler/sessions_handler/sessions_handler.go
+++ handler/sessions_handler/sessions_handler.go
@@ -4,14 +4,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/derUbermenk/go-user_auth_api/service/session_service"
 	"github.com/gin-gonic/gin"
 )
 
-func Create(ss *session_service.SessionService) func(c *gin.Context) {
+func Create(ss session_service.SessionService) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		var new_session_request session_service.NewSessionRequest
 
-		_ = c.ShouldBindJSON(ss.CreateUser(new_session_request))
+		_ = c.ShouldBindJSON(session_service.new_session_request)
 
 		id, valid_credentials, err := ss.CreateSession(new_session_request)
 
