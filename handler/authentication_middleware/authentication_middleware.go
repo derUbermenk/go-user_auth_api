3463ab87493a601df5e50618d988a9508f10c56d
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-06 22:11:02 +0800

    implement Authenticate function for Authentication Middleware

--- handler/authentication_middleware/authentication_middleware.go
+++ handler/authentication_middleware/authentication_middleware.go
@@ -1,17 +1,27 @@
 package authentication_middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // checks if client is logged in
 func Authenticate() func(c *gin.Context) {
 	return func(c *gin.Context) {
+		session_cookie, err := c.Request.Cookie("session")
 
+		if err != nil {
+			c.Status(http.StatusUnauthorized)
+			return
+		}
+
+		c.Set("user_id", session_cookie.Value)
+		c.Next()
 	}
 }
 
 // checks if the client owns the resource
 func AuthorizeOwner() func(c *gin.Context) {
-	return func(c *gin.Context) {
-
-	}
+	return func(c *gin.Context) {}
 }
