d49455b22a1938b4c7112b393abd4e3cbc192344
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-06 17:17:53 +0800

    add NewSessionRequest struct

--- service/session_service/session_service.go
+++ service/session_service/session_service.go
@@ -1,5 +1,10 @@
 package session_service
 
+type NewSessionRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 type SessionService interface {
 	CreateSession(request NewSessionRequest) (id int, valid_credentials bool, err error)
 }
