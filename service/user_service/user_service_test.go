5ac2d6a8cdd9b3ff3a09b12cb0ed723306101bad
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-07 20:31:36 +0800

    add tests for UserService Create function

--- service/user_service/user_service_test.go
+++ service/user_service/user_service_test.go
@@ -8,5 +8,40 @@ import (
 )
 
 var _ = Describe("UserService", func() {
+	us := userService.UserServiceStruct(UserRepositoryDouble{})
 
+
+	Describe("CreateUser", func() {
+		Context("when user creation succeeded", func() {
+			It("returns a user, success true, and err nil", func() {
+				new_user_request := user_service.NewUserRequest{
+					Email:    "valid_email@email.com",
+					Name:     "valid name",
+					Password: "valid_password",
+				}
+
+				user, success, err := us.Create(new_user_request)
+
+				Expect(user).To(Not(BeNil()))
+				Expect(success).To(BeTrue())
+				Expect(err).To(BeNil())
+			})
+		})
+
+		Context("when there is a missing required param", func() {
+			It("returns a nil user, success false, and err nil") {
+				new_user_request := user_service.NewUserRequest{
+					Email:    "valid_email@email.com",
+					Name:     "valid name",
+					// missing password
+				}
+
+				user, success, err := us.Create(new_user_request)
+
+				Expect(user).To(Not(BeNil()))
+				Expect(success).To(BeTrue())
+				Expect(err).To(BeNil())
+			}
+		})
+	})
 })
