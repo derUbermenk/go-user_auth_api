6acd5d0020ce4db0f3c3514f46dbdf46c9b1076c
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-08 21:18:56 +0800

    add test files

---
+++ repository/user_repository/user_repository_test.go
@@ -0,0 +1,18 @@
+package user_repository_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("UserRepository", func() {
+	Describe("Create", func() {
+		It("creates a new user record given valid values", func() {
+
+		})
+
+		It("creates a new user record given valid values", func() {
+
+		})
+	})
+})
