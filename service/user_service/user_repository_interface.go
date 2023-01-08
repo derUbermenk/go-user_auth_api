86f2a97d67f2bd0ca65fe36249e45e7468d760db
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-08 16:41:40 +0800

    change package of User from user_repository to repository

--- service/user_service/user_repository_interface.go
+++ service/user_service/user_repository_interface.go
@@ -1,22 +1,22 @@
 package user_service
 
-import "github.com/derUbermenk/go-user_auth_api/repository/user_repository"
+import "github.com/derUbermenk/go-user_auth_api/repository"
 
 type UserRepositoryInterface interface {
-	Create(user_info map[string]interface{}) (user user_repository.User, err error)
-	FindByEmail(email string) (user user_repository.User, err error)
-	FindPublic(id int) (user user_repository.User, err error)
-	FindPrivate(id int) (user user_repository.User, err error)
-	Delete(id int) (deleted_user user_repository.User, err error)
+	Create(user_info map[string]interface{}) (user repository.User, err error)
+	FindByEmail(email string) (user repository.User, err error)
+	FindPublic(id int) (user repository.User, err error)
+	FindPrivate(id int) (user repository.User, err error)
+	Delete(id int) (deleted_user repository.User, err error)
 }
 
 type UserRepositoryDouble struct{}
 
-func (u *UserRepositoryDouble) Create(user_info map[string]interface{}) (user user_repository.User, err error) {
+func (u *UserRepositoryDouble) Create(user_info map[string]interface{}) (user repository.User, err error) {
 	return
 }
 
-func (u *UserRepositoryDouble) FindByEmail(email string) (user user_repository.User, err error) {
+func (u *UserRepositoryDouble) FindByEmail(email string) (user repository.User, err error) {
 	if email == "existing_user_email@email.com" {
 		user.ID = 1
 		return
@@ -25,7 +25,7 @@ func (u *UserRepositoryDouble) FindByEmail(email string) (user user_repository.U
 	return
 }
 
-func (u *UserRepositoryDouble) FindPublic(id int) (user user_repository.User, err error) {
+func (u *UserRepositoryDouble) FindPublic(id int) (user repository.User, err error) {
 	if id == 1 {
 		user.ID = 1
 		return
@@ -34,7 +34,7 @@ func (u *UserRepositoryDouble) FindPublic(id int) (user user_repository.User, er
 	return
 }
 
-func (u *UserRepositoryDouble) FindPrivate(id int) (user user_repository.User, err error) {
+func (u *UserRepositoryDouble) FindPrivate(id int) (user repository.User, err error) {
 	if id == 1 {
 		user.ID = 1
 		return
@@ -43,7 +43,7 @@ func (u *UserRepositoryDouble) FindPrivate(id int) (user user_repository.User, e
 	return
 }
 
-func (u *UserRepositoryDouble) Delete(id int) (deleted_user user_repository.User, err error) {
+func (u *UserRepositoryDouble) Delete(id int) (deleted_user repository.User, err error) {
 	if id == 1 {
 		deleted_user.ID = 1
 
