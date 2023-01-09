c77e88d2387a11b76a40782e941578a758d96965
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-09 18:56:41 +0800

    write initial setup code for user repository tests

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.2
 	github.com/goccy/go-json v0.9.11
+	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.0
 	github.com/onsi/ginkgo/v2 v2.6.1
 	github.com/onsi/gomega v1.24.2
 )
@@ -21,7 +23,8 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
--- repository/user_repository/user_repository_test.go
+++ repository/user_repository/user_repository_test.go
@@ -1,18 +1,101 @@
 package user_repository_test
 
 import (
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/derUbermenk/go-user_auth_api/repository/user_repository"
+	_ "github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/source/file"
+	_ "github.com/lib/pq"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
+func CreateDB() {
+	cmd := exec.Command("createdb", "user_authed_api_test")
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+
+	if err != nil {
+		log.Printf("%v\n", err.Error())
+		AbortSuite(err.Error())
+	}
+}
+
+func DropDB() {
+	cmd := exec.Command("dropdb", "user_authed_api_test")
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+
+	if err != nil {
+		AbortSuite(err.Error())
+	}
+}
+
+func RunMigrations() {
+
+	/*
+		m, err := migrate.New(
+			"file:/home/chester/Documents/code_projects/go-projects/user_auth_api/repository/migrations",
+			"postgres://chester:baba_yetu@localhost:5432/user_authed_api_test?sslmode=disable",
+		)
+
+		if err != nil {
+			AbortSuite(err.Error())
+		}
+
+		if err := m.Up(); err != nil {
+			AbortSuite(err.Error())
+		}
+	*/
+
+	// hacky way, above does not allow drop database
+
+	migrations_path := "file:/home/chester/Documents/code_projects/go-projects/user_auth_api/repository/migrations"
+	db_connstring := "postgres://chester:baba_yetu@localhost:5432/user_authed_api_test?sslmode=disable"
+
+	cmd := exec.Command("migrate", "-source", migrations_path, "-database", db_connstring, "up")
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+
+	if err != nil {
+		AbortSuite(err.Error())
+	}
+}
+
 var _ = Describe("UserRepository", func() {
+	// var db *sqlx.DB
+
 	Describe("Create", func() {
-		It("creates a new user record given valid values", func() {
+		BeforeEach(func() {
+			CreateDB()
+			RunMigrations()
+		})
 
+		AfterEach(func() {
+			DropDB()
+		})
+
+		It("creates a new user record given valid values", func() {
+			_ = user_repository.NewUserRepository(db)
 		})
 
 		It("creates a new user record given valid values", func() {
 
 		})
+
+		// after reset db
 	})
 })
