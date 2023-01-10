622f4044d98befb8d598d1bbbd7d71e7f0eb3c22
Chester Asuncion <asuncionmcv@gmail.com>
2023-01-10 19:46:20 +0800

    implement Create

--- repository/user_repository/user_repository.go
+++ repository/user_repository/user_repository.go
@@ -1,6 +1,9 @@
 package user_repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/derUbermenk/go-user_auth_api/repository"
+	"github.com/jmoiron/sqlx"
+)
 
 type userrepository struct {
 	db *sqlx.DB
@@ -8,4 +11,22 @@ type userrepository struct {
 
 func NewUserRepository(db *sqlx.DB) (uR *userrepository) {
 	return &userrepository{db: db}
+}
+
+func (u *userrepository) Create(userInfo map[string]interface{}) (user repository.User, err error) {
+	tx := u.db.MustBegin()
+
+	err = tx.QueryRow(`INSERT INTO users (password,email) VALUES ($1, $2) RETURNING id`, userInfo["password"], userInfo["email"]).Scan(&user.ID)
+
+	if err != nil {
+		tx.Rollback()
+		return
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return
+	}
+
+	return
 }
